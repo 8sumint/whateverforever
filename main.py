55921b5c7618a39b6d15d566eb7d8f7db6049301
8sumint <celeste@natu.moe>
2025-02-03 18:09:31 -0500

    initial commit

---
+++ main.py
@@ -0,0 +1,36 @@
+from flask import Flask, request
+from yt_dlp import YoutubeDL
+import os
+
+app = Flask(__name__)
+
+base_dir = "M:\\Archive\\yt all\\"
+
+@app.route("/archive")
+def do_archive():
+	url = request.args.get("url")
+	if not url:
+		return "?url=..."
+
+	ydl_opts = {
+		'writethumbnail': True,
+		'writesubtitles': True,
+		'writeinfojson': True,
+		'getcomments': True,
+		'embedchapters': True,
+		'format_sort': ['res', 'codec:av1'],
+		'outtmpl': os.path.join(base_dir, '%(webpage_url_domain)s/%(uploader)s/[%(upload_date)s] %(title)s [%(id)s].%(ext)s')
+	}
+
+	with YoutubeDL(ydl_opts) as ydl:
+		try:
+			ydl.download([url])
+		except Exception as e:
+			return f"error: {str(e)}"
+
+	return "ok"
+
+
+@app.route("/test")
+def do_test():
+	return "test"
